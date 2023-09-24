74fc9830fa37edd1ebca19819414be1cd3509889
Raul Ernesto Guillen Hernandez <00012119@uca.edu.sv>
2023-09-23 21:27:01 -0600

    Add Java Project

---
+++ Declarativa/src/main/java/com/mycompany/declarativa/Declarativa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.declarativa;
+
+/**
+ *
+ * @author kira2
+ */
+public class Declarativa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
